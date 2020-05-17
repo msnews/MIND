8eb49e1a01b71c9da4851b5c754ae95a605a4cf9
Joan Lily <joanlilyqy@hotmail.com>
2020-05-17 16:35:57 -0700

    local test with html basic

--- crawler/msn/spiders/msn_spider.py
+++ crawler/msn/spiders/msn_spider.py
@@ -8,8 +8,18 @@ class MSNSpider(scrapy.Spider):
     allowed_domains = ["msn.com"]
 
     #start_urls = ["https://www.msn.com/en-us/sports/football_nfl/pittsburgh-steelers-well-represented-in-the-top-10-of-the-nfl's-top-100-teams/ar-BBWRHuM?ocid=chopendata"]
-    start_urls = ["file:D:\Repo\MIND\crawler\\test_sample\BBWRHuM.mhtml"]
+    start_urls = ["file:D:\Repo\MIND\crawler\\test_sample\BBWRHuM.html"]
+    
+    def parse(self, response):
+        print(response)
+        item = NewsItem()
+        item['body'] = response.css('.articlecontent p::text, \
+                                         .articlecontent p a::text, \
+                                         .articlecontent blockquote::text, \
+                                         .articlecontent blockquote a::text').getall()
+        yield item
 
+    """
     def parse(self, response):
 
         url = unquote(response.url)
@@ -36,5 +46,5 @@ class MSNSpider(scrapy.Spider):
 
 
         yield item
-         
+    """
         
